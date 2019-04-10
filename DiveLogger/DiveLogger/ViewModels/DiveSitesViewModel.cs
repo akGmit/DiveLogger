aa57c8dd9ac7d39e882d9baea99b1df0cf8b4e48
akGMIT <G00328529@gmit.ie>
2019-04-10 18:24:20 +0100

    Getting user current location and viewing it on map

--- DiveLogger/DiveLogger/ViewModels/DiveSitesViewModel.cs
+++ DiveLogger/DiveLogger/ViewModels/DiveSitesViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
 namespace DiveLogger.ViewModels
@@ -8,11 +12,62 @@ namespace DiveLogger.ViewModels
     
     class DiveSitesViewModel : BaseViewModel
     {
-        private Map sitesMap;
-        public Map SitesMap
+        private Xamarin.Forms.Maps.Map mapSites;
+        private StackLayout stack;
+        public StackLayout Stack
         {
-            get => sitesMap;
-            set => SetValue(ref sitesMap, value);
+            get => stack;
+            set
+            { SetValue(ref stack, value); }
+        }
+        public DiveSitesViewModel()
+        {
+            InitializeMapAsync();
+
+            //Initialize stack
+            stack = new StackLayout();
+            
+            stack.Children.Add(mapSites);
+        }
+
+        private async void InitializeMapAsync()
+        {
+            Location location = new Location();
+            try
+            {
+                location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            } 
+            mapSites = new Xamarin.Forms.Maps.Map(
+            MapSpan.FromCenterAndRadius(
+                    new Position(location.Latitude, location.Longitude), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                MapType = MapType.Satellite
+            };
         }
     }
 }
