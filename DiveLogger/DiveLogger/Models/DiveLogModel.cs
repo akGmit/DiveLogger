a1f1b96a80e18d7f60928973125a173d71d27a09
akgmit <andrius.korsakas@gmail.com>
2019-04-15 23:43:23 +0100

    Apropratie variable types for DiveLogModel

--- DiveLogger/DiveLogger/Models/DiveLogModel.cs
+++ DiveLogger/DiveLogger/Models/DiveLogModel.cs
@@ -4,89 +4,34 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 using Xamarin.Essentials;
-
+/// <summary>
+/// Dive log model.
+/// </summary>
 namespace DiveLogger.Models
 {
-    public class DiveLogModel : BaseViewModel
+    public class DiveLogModel
     {
 
-        #region Properties
-
-        private string diveNumber;
-        private string visibility;
-        private string maxDepth;
-        private string duration;
-        private string waterTemp;
-        private string totalDiveTime;
-        private string diveNotes;
-        private string location;
-
-        public string DiveNumber
-        {
-            get => diveNumber;
-            set { SetValue(ref diveNumber, value); }
-        }
-        public string Visibility
-        {
-            get => visibility;
-            set { SetValue(ref visibility, value); }
-        }
-        public string MaxDepth
-        {
-            get => maxDepth;
-            set { SetValue(ref maxDepth, value); }
-        }
-        public string Duration
-        {
-            get => duration;
-            set { SetValue(ref duration, value); }
-        }
-        public string WaterTemp
-        {
-            get => waterTemp;
-            set { SetValue(ref waterTemp, value); }
-        }
-        public string TotalDiveTime
-        {
-            get => totalDiveTime;
-            set { SetValue(ref totalDiveTime, value); }
-        }
-        public string DiveNotes
-        {
-            get => diveNotes;
-            set { SetValue(ref diveNotes, value); }
-        }
-        public string Location
-        {
-            get => location;
-            set { SetValue(ref location, value); }
-        }
+        #region Variables
+        public int DiveNumber { get; set; }
+        public double Visibility { get; set; }
+        public double MaxDepth { get; set; }
+        public TimeSpan Duration { get; set; }
+        public double  WaterTemp { get; set; }
+        public string DiveNotes { get; set; }
+        public string Location { get; set; }
 
         #endregion
-        //private DiveLogModel diveLog;
-        //public DiveLogModel DiveLog
-        //{
-        //    get => diveLog;
-        //    set
-        //    {
-        //        SetValue(ref diveLog, value);
-        //    }
-        //}
 
-        public DiveLogModel()
-        {
-        }
-
-        public DiveLogModel(string diveNumber, string visibility, string maxDepth,
-            string duration, string waterTemp,
-                string totalDiveTime, string diveNotes, string location)
+        public DiveLogModel(int diveNumber, double visibility, double maxDepth,
+            TimeSpan duration, double waterTemp,
+                string diveNotes, string location)
         {
             DiveNumber = diveNumber;
             Visibility = visibility;
             MaxDepth = maxDepth;
             Duration = duration;
             WaterTemp = waterTemp;
-            TotalDiveTime = totalDiveTime;
             DiveNotes = diveNotes;
             Location = location;
         }
