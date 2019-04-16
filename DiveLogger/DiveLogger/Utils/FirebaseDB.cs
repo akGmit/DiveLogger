22b30b9b0b0bc351ed401a9e5156f276d6f3aef0
akgmit <andrius.korsakas@gmail.com>
2019-04-16 01:24:33 +0100

    FirebaseDB util: InitFirebaseClient , AuthAsync()

---
+++ DiveLogger/DiveLogger/Utils/FirebaseDB.cs
@@ -0,0 +1,28 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiveLogger.Utils
+{
+    
+    class FirebaseDB
+    {
+        private const string BaseDBUri = "https://diveloggerapp.firebaseio.com";
+
+        public FirebaseClient InitFirebaseClient()
+        {
+            return new FirebaseClient(BaseDBUri, new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => AuthAsync()
+            });
+        }
+
+        private Task<string> AuthAsync()
+        {
+            FireBaseAuthUtil auth = new FireBaseAuthUtil();
+            return auth.Login("inti", "init");
+        }
+    }
+}
